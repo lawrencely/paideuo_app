7f91ac8d7b5a8d0a5550088dc229e8ee2d6a6c90
lawrencely <lawrence.ly667@gmail.com>
2014-10-04 20:15:56 +1000

    added categories link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
       nav += "<li>" + link_to('All users', users_path) + "</li> "
       nav += "<li>" + link_to('Edit profile', edit_user_path) + "</li> "
       nav += "<li>#{ link_to('Sign out', login_path, :method => :delete, :data => {:confirm => 'Are you sure?'}) }</li>"
-      nav += "<li>" + link_to('Create Category', new_category ) + "</li>"
+      nav += "<li>" + link_to('Create Category', new_category_path ) + "</li>"
     else
       nav += "<li>#{ link_to('Sign up', new_user_path) }</li> "
       nav += "<li>#{ link_to('Sign in', login_path) }</li>"
