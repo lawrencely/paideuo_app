7d9ccd2fd5750ccbeb437141214e95b0bdf4f63d
lawrencely <lawrence.ly667@gmail.com>
2014-10-05 14:33:16 +1100

    going to start adding tasks

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,18 +1,23 @@
 class CategoriesController < ApplicationController
 
   def index
-    @categories = Category.all
+    if @current_user.present?
+    @category = Category.where :user_id => @current_user.id
+  else
+    redirect_to root_path
+  end
   end
 
   def new
+    blocker
     @category = Category.new
   end
 
   def create
     @category = Category.new category_params
-    @current_user
-    if @category.save
-        redirect_to categories_path
+    if @category.user_id = @current_user.id
+      @category.save
+      redirect_to categories_path
     else
       render :new
     end
@@ -47,6 +52,14 @@ class CategoriesController < ApplicationController
   private
   def category_params
     params.require(:category).permit(:heading, :user_id)
+  end
+
+  def blocker
+    if @current_user.present?
+    else
+      flash[:notice] = "You must be registered to do anything stop trying to break me"
+      redirect_to root_path
+    end
   end
 
 end
