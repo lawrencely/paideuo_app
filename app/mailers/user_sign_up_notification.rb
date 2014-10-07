2e5cb7fd5e85ce7ad31356f112958fd93cb2999f
lawrencely <lawrence.ly667@gmail.com>
2014-10-07 23:39:13 +1100

    fuck this shit

--- app/mailers/user_sign_up_notification.rb
+++ app/mailers/user_sign_up_notification.rb
@@ -1,9 +1,8 @@
 class UserSignUpNotification < ActionMailer::Base
-  default from: "lawrence.wdi6@gmail.com"
+  default from: "lawrence.ly667@gmail.com"
 
   def welcome_email(user)
     @user = user
-    @url = 'http://example.co'
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
 end
