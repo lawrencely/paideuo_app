00f0fe6d026b1263849ee64206ac8870e6ec899d
lawrencely <lawrence.ly667@gmail.com>
2014-10-08 14:46:40 +1100

    removed validation for task

--- app/models/task.rb
+++ app/models/task.rb
@@ -15,6 +15,6 @@
 class Task < ActiveRecord::Base
   belongs_to :category
   belongs_to :partner
-  validates :paideuo, :presence => true, :length => { :in => 3..20}
+  validates :paideuo, :presence => true,
   validates :datetime, :presence => true
 end
