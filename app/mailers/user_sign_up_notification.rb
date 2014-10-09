7cd03f8346b7acec68710fa8c18607bde591624f
lawrencely <lawrence.ly667@gmail.com>
2014-10-10 10:37:13 +1100

    boostrap and email editing

--- app/mailers/user_sign_up_notification.rb
+++ app/mailers/user_sign_up_notification.rb
@@ -4,11 +4,11 @@ class UserSignUpNotification < ActionMailer::Base
   def welcome_email(user)
     @user = user
     @url = 'http://paideuo.herokuapp.com/login'
-    mail(to: @user.email, subject: 'Welcome <%= @user.name %>')
+    mail(to: @user.email, subject: 'Welcome')
   end
 
   def paideuo_email(partner)
     @partner = partner
-    mail(to: @partner.email, subject: '<%= @partner.name %>, Paideuo Time')
+    mail(to: @partner.email, subject: 'Paideuo Time')
   end
 end
